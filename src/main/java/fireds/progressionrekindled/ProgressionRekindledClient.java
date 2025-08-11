535714188b80dd311eb7ad991740f2d001e72966
Noah Hozian <nthozian@gmail.com>
2025-08-11 01:19:31 -0500

    Directory/File Setup Stopped due to needing Java 21 to download the source of the MinecraftServer.class

---
+++ src/main/java/fireds/progressionrekindled/ProgressionRekindledClient.java
@@ -0,0 +1,10 @@
+package fireds.progressionrekindled;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProgressionRekindledClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
